beace094d705f5ad6c1fe330c8cf9b6b6c2b01b6
Richard Gagg <r.gagg@outlook.com>
2025-02-12 08:35:07 +1100

    Initial Commit

---
+++ Magic8Ball/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Magic8Ball
+//
+//  Created by Richard Gagg on 12/2/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Magic8Ball/Magic8BallApp.swift
@@ -0,0 +1,17 @@
+//
+//  Magic8BallApp.swift
+//  Magic8Ball
+//
+//  Created by Richard Gagg on 12/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct Magic8BallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
