52a5270514669154ef2456ec0483541ff27a894c
Richard Gagg <r.gagg@outlook.com>
2025-02-12 13:56:37 +1100

    Updated to have a haptic vibration on button click

--- Magic8Ball/ContentView.swift
+++ Magic8Ball/ContentView.swift
@@ -6,11 +6,13 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct ContentView: View {
   @State private var predictMessage: String = ""
   @State private var newMessageIndex: Int = -1
   @State private var lastMessageIndex: Int = -1
+  private let impactGenerator = UIImpactFeedbackGenerator(style: .heavy)
   
   private let choicesArray: [String] = [
     "It is certain",
@@ -42,33 +44,44 @@ struct ContentView: View {
       VStack {
         Spacer()
         
-        Image("predict-ball-image")
-          .resizable()
-          .scaledToFit()
-          .frame(width: 350, height: 350)
+        ZStack {
+          Image("predict-ball-image")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 350, height: 350)
+          
+          Circle()
+            .foregroundStyle(.indigo)
+            .frame(height: 130)
+            .offset(y: -15)
+            
         
         Text(predictMessage)
           .font(.largeTitle)
-          .foregroundStyle(.black)
+          .foregroundStyle(.white)
           .multilineTextAlignment(.center)
-          .minimumScaleFactor(0.5)
-          .frame(height: 120)
+          .minimumScaleFactor(0.4)
+          .frame(width: 100, height: 100)
+          .offset(y: -15)
           .animation(
             .easeInOut(duration: 0.5)
             .delay(0.25),
             value: predictMessage
           )
+          
+        }
         Spacer()
         
         Button {
-          
+          impactGenerator.impactOccurred()
+
           repeat {
             newMessageIndex = Int.random(in: 0...choicesArray.count - 1)
           } while lastMessageIndex == newMessageIndex
           
           predictMessage = (choicesArray[newMessageIndex])
           lastMessageIndex = newMessageIndex
-          
+                    
         } label: {
           Text("Hit Me!")
             .font(.title2)
